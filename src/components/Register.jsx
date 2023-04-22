9b6c345d08cd30c759c0f0b81a3b68eb159aa7d3
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-04-22 22:12:31 +0600

    update the code

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -90,25 +90,6 @@ const Register = () => {
               </div>
             </div>
 
-            {/* <div>
-              <label
-                htmlFor="confirm-password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Confirm Password
-              </label>
-              <div className="mt-1">
-                <input
-                  id="confirm-password"
-                  name="confirm-password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-              </div>
-            </div> */}
-
             <div>
               <label>
                 <Link to="/login" className="btn-link">
